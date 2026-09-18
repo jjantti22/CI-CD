0b54a0b9a6e2442c4ac24b4d2fa23aabd685be91
weed <weed@localhost.localdomain>
2026-09-18 06:37:39 +0300

    Exercise 9 fix

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from '@playwright/test';
-export default defineConfig({
+const { defineConfig } = require('@playwright/test')
+module.exports = defineConfig({
   testDir: './e2e-tests',
+  testMatch: '**/*.spec.js',
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:8080',
+    url: 'http://127.0.0.1:8080',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
     baseURL: 'http://localhost:8080/',
   },
-});
+})
