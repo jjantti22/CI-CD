34a4d3eb719cb239e34402d3f81d284fdbf8c96e
weed <weed@localhost.localdomain>
2026-09-22 14:25:34 +0300

    Exercise 12-1

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const start = async () => {
   console.log(`server started on port ${PORT}`)
 }
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 start()
