fcf619c87e79bbbb48a2205e9b43159a982c7b92
weed <weed@localhost.localdomain>
2026-09-22 13:54:24 +0300

    Exercise 10v2

--- app.js
+++ app.js
@@ -10,5 +10,8 @@ const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
 }
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 start()
